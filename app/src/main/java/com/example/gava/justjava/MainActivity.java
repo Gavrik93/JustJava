4d8aa5151a78115cde868a01ffeffc069e81ce80
Gavrik93 <777gava777@gmail.com>
2019-02-04 15:21:24 +0200

    Add Coffee price input and Button submit for this input

--- app/src/main/java/com/example/gava/justjava/MainActivity.java
+++ app/src/main/java/com/example/gava/justjava/MainActivity.java
@@ -26,31 +26,51 @@ package com.example.gava.justjava;
 
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button priceButton;
+    int coffeePrice;
+
+    EditText coffeePriceInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coffeePriceInput = (EditText) findViewById(R.id.coffeePrice);
+
+        priceButton = (Button) findViewById(R.id.priceButton);
+        priceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                coffeePrice = Integer.valueOf(coffeePriceInput.getText().toString());
+            }
+        });
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     int numberOfCoffees = 0;
+
+
     public void submitOrder(View view) {
-        String priceMessage = "You owe "+ (numberOfCoffees*7) + "bucks, dude" + "\n Thank you! ";
+        String priceMessage = "You owe "+ (numberOfCoffees*coffeePrice) + "bucks, dude" + "\n Thank you! ";
         displayMessage(priceMessage);
 //        display(numberOfCoffees);
 //        displayPrice(numberOfCoffees*10);
     }
+
+
+
     /*This metod change increment*/
     public void increment(View view) {
         numberOfCoffees++;
@@ -61,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         if (numberOfCoffees > 0) numberOfCoffees--;
         display(numberOfCoffees);
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
