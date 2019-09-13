4fdf4c8d6b7ebcf9de6fc1fcce9d9bd2e9905f5e
E.Gajvoronskij <777gava777@gmail.com>
2019-09-13 17:32:45 +0300

    adding Toast massage for quantity and adding mail intent. cool

--- app/src/main/java/com/example/gava/justjava/MainActivity.java
+++ app/src/main/java/com/example/gava/justjava/MainActivity.java
@@ -2,12 +2,15 @@
 package com.example.gava.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
      */
     int quantity = 0;
 
-
+    // processing button Order
     public void submitOrder(View view) {
         EditText nameField = findViewById(R.id.nameField);
         String name = nameField.getText().toString();
@@ -39,18 +42,33 @@ public class MainActivity extends AppCompatActivity {
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
 
-        int price = calculatePrice();
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
         String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
         displayMessage(priceMessage);
 
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order coffee" + name);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * Calculates the price of the order.
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+
+        int basePrice = 5;
+        if (hasWhippedCream){
+            basePrice = basePrice +1;
+        }
+        if (hasChocolate){
+            basePrice = basePrice + 2;
+        }
+
+        return quantity * basePrice;
     }
 
     /**
@@ -76,13 +94,22 @@ public class MainActivity extends AppCompatActivity {
 
     /*This metod change increment*/
     public void increment(View view) {
+        if (quantity == 50) {
+            Toast.makeText(this, "You cannot have more then 50 cups of coffees",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         quantity++;
         displayQuanity(quantity);
     }
 
     /*This metod change decrement*/
     public void decrement(View view) {
-        if (quantity > 0) quantity--;
+        if (quantity >= 0) {
+            Toast.makeText(this, "You cannot have less then 0 cup of coffee",Toast.LENGTH_SHORT).show();
+            return;
+        }
+            quantity--;
         displayQuanity(quantity);
     }
 
